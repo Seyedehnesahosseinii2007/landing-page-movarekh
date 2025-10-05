2a12bd2e256c5247672c5894d579a7779600da43
Seyedeh Nesa Hosseini <seyedehnesahosseiniofficiall@outlook.com>
2025-10-05 16:04:27 +0330

    finish

---
+++ src/javascript/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var els = document.querySelectorAll(".detail-form, .people-two, .people-one, .title-container");
+  var io = new IntersectionObserver(
+    function (entries, obs) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("in-view");
+          obs.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+  els.forEach((el) => io.observe(el));
+});
